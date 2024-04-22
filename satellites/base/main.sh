a7d949c392112622a2a5f8bfed3085812aa51c48
vladospuchkov <v.puchkov@g.nsu.ru>
2024-04-22 21:03:47 +0700

    add labs

--- satellites/base/main.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-DATA_DIR=../data
-SUBMISSION_PATH=/code_execution/submission/submission.csv
-
-# call our script (main.py in this case) and tell it where the data is and
-python main.py $DATA_DIR $SUBMISSION_PATH
